342e0b8fac0df96477496e7f64abf3740b11f57a
SMenon@walmartlabs.com <SMenon@walmartlabs.com>
2017-06-26 16:27:44 +0530

    first commit

---
+++ app/src/main/java/example/com/restcaller/IP.java
@@ -0,0 +1,17 @@
+package example.com.restcaller;
+
+/**
+ * Created by smenon2 on 11/8/15.
+ */
+public class IP {
+
+    private String ip;
+
+    public String getIp() {
+        return ip;
+    }
+
+    public void setIp(String ip) {
+        this.ip = ip;
+    }
+}
